fb578c62a6e31ab23902933f3c68fb3ab8428362
Jordan <jmath0810@gmail.com>
2022-09-30 10:45:52 +1000

    fixed main fucntion

--- main.py
+++ main.py
@@ -3,43 +3,46 @@ from pygame.locals import *
 import sys
 pygame
 
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 600
+def main():
+    SCREEN_WIDTH = 600
+    SCREEN_HEIGHT = 600
 
-FPS = 60
+    FPS = 60
 
-pSizeX = 30
-pSizeY = 30
+    pSizeX = 30
+    pSizeY = 30
 
-BLUE = (0, 0, 255)
+    BLUE = (0, 0, 255)
 
-myScreen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Example 1")
-
-
-clock = pygame.time.Clock()
+    myScreen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Example 1")
 
 
-moveLeft = False
-moveRight = False
-moveUp = False
-moveDown = False
+    clock = pygame.time.Clock()
+
+
+    moveLeft = False
+    moveRight = False
+    moveUp = False
+    moveDown = False
 
 
 
-# (x, y) 
+    # (x, y) 
 
 
-running = True
-while running:
-    
-    clock.tick(60)
-    
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: 
-            running = False
-        myScreen.fill(BLUE)    
-        pygame.display.update() 
-    
+    running = True
+    while running:
+        
+        clock.tick(60)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: 
+                running = False
+            myScreen.fill(BLUE)    
+            pygame.display.update() 
+        
     # pygame.display.flip()
     
+
+if __name__ == '__main__': main()
